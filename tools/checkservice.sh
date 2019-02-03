b4f75d2cca2941a0d594a286c6d21d67b8c197c9
root <root@ip-172-31-42-174.us-east-2.compute.internal>
2019-02-03 10:58:23 +0300

    adding tools

---
+++ tools/checkservice.sh
@@ -0,0 +1,10 @@
+echo -e "Status of the services for IoT platform: \n"
+echo " MQTT  broker server:"
+sudo service mosquitto status | grep 'Active'
+echo "----------------------------------"
+echo " Database status:"
+sudo service mysql status | grep 'Active'
+echo "----------------------------------"
+echo " Openhab2 server status:"
+sudo service openhab2 status | grep 'Active'
+echo "----------------------------------"
