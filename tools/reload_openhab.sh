e5bf0f7619e9672ec6fb8c3efd841ae1a055fcb4
root <root@ip-172-31-42-174.us-east-2.compute.internal>
2019-02-17 15:11:17 +0300

    corrected rload script for the tools folder

--- tools/reload_openhab.sh
+++ tools/reload_openhab.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+echo 'Start of the script'
 cd /usr/local/projects
-sudo git pull local master
-echo 'Configuration reloaded'
+pwd
+sudo git pull origin master
+echo 'Loading the configuration done'
 sudo service openhab2 restart
-echo 'Server reload completed'
+echo 'Server restart completed, waiting log for errors'
+sleep 15s
+tail -n 50 /var/log/openhab2/openhab.log | grep 'ERROR'
