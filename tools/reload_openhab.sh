df2476bb60a9407f576f63d86e4805f17091d7e0
DragotaC <DragotaC@E0394784.internal.vodafone.com>
2019-02-20 10:24:35 +0300

    corrected reload script

--- tools/reload_openhab.sh
+++ tools/reload_openhab.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo 'Start of the script'
 cd /usr/local/projects
-pwd
 sudo git pull origin master
 echo 'Loading the configuration done'
-sudo service openhab2 restart
-echo 'Server restart completed, waiting log for errors'
-sleep 15s
-tail -n 50 /var/log/openhab2/openhab.log | grep 'ERROR'
+sleep 2s
+echo 'Below changes reloaded by server'
+tail -n 10 /var/log/openhab2/openhab.log | grep 'INFO'
