061037798eeaccd2c7852147e8ad2253d8b9b3e9
DragotaC <DragotaC@E0394784.internal.vodafone.com>
2019-03-13 12:12:24 +0300

    added living in the automation test

--- tools/push_mqtt.py
+++ tools/push_mqtt.py
@@ -8,10 +8,10 @@ def on_message(client, userdata, message):
     print("message topic=",message.topic)
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
-    if message.topic == "QQ010/G0/bedroom/switch1/set":
+    if "switch1/set" in message.topic:
         set_switch1 = str(message.payload.decode("utf-8"))
         print("message received, the unit is: " ,set_switch1)
-    if message.topic == "QQ010/G0/bedroom/temp1/get":
+    if "temp1/get" in message.topic:
         get_temp1 = str(message.payload.decode("utf-8"))
         print("message received, the unit is: " ,get_temp1)
 
@@ -41,4 +41,18 @@ if set_switch1 == "ON":
     new_temp1 = float(get_temp1) + (random.randint(0,5)*0.05)
 print("Calculated value is:",str("%.2f" % new_temp1))
 mqttpush.publish("QQ010/G0/bedroom/temp1/get",str("%.2f" % new_temp1),retain=True)
+mqttpush.unsubscribe("QQ010/G0/bedroom/temp1/get")
+mqttpush.unsubscribe("QQ010/G0/bedroom/switch1/set")
+############## New entry ############
+print("Subscribing to topics: ","QQ010/G0/livingroom/temp1/set")
+mqttpush.subscribe("QQ010/G0/livingroom/temp1/get")
+mqttpush.subscribe("QQ010/G0/livingroom/switch1/set")
+time.sleep(2)
+print("Publish to topic: ","QQ010/G0/livingroom/temp1/get")
+if set_switch1 == "OFF":
+    new_temp1 = float(get_temp1) - (random.randint(0,2)*0.05)
+if set_switch1 == "ON":
+    new_temp1 = float(get_temp1) + (random.randint(0,5)*0.05)
+print("Calculated value is:",str("%.2f" % new_temp1))
+mqttpush.publish("QQ010/G0/livingroom/temp1/get",str("%.2f" % new_temp1),retain=True)
 mqttpush.loop_stop()
